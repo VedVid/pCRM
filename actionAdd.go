e74ab67b39ee760acceec97695e7160a9f5c45ee
Tomasz Nowakowski <toalak@gmail.com>
2021-04-03 23:50:01 +0200

    Basic structure

---
+++ actionAdd.go
@@ -0,0 +1,78 @@
+/*
+Copyright (c) 2021, Tomasz "VedVid" Nowakowski
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+package main
+
+
+import (
+	"fmt"
+)
+
+
+func ActionAdd(forename, fn, surname, sn, birthdate, bd string) {
+
+	if forename == "" && fn == "" && surname == "" && sn == "" &&
+		birthdate == "" && bd == "" {
+		fmt.Println("INFO: Switching to interactive mode.")
+		}
+
+	switch {
+	case len(forename) == 0 && len(fn) == 0:
+		break
+	case forename == fn:
+		fmt.Println("WARNING: You used two flags to set forename.")
+		break
+	case (len(forename) > 0 && len(fn) == 0) || (len(forename) == 0 && len(fn) > 0):
+		fmt.Println("INFO: Proper data entered.")
+	default:
+		fmt.Println("ERROR: You used two flags to set forename, each with different data.")
+	}
+
+	switch {
+	case len(surname) == 0 && len(sn) == 0:
+		break
+	case surname == sn:
+		fmt.Println("WARNING: You used two flags to set surname.")
+		break
+	case (len(surname) > 0 && len(sn) == 0) || (len(surname) == 0 && len(sn) > 0):
+		fmt.Println("INFO: Proper data entered.")
+	default:
+		fmt.Println("ERROR: You used two flags to set surname, each with different data.")
+	}
+
+	switch {
+	case len(birthdate) == 0 && len(bd) == 0:
+		break
+	case birthdate == bd:
+		fmt.Println("WARNING: You used two flags to set birthdate.")
+		break
+	case (len(birthdate) > 0 && len(bd) == 0) || (len(birthdate) == 0 && len(bd) > 0):
+		fmt.Println("INFO: Proper data entered.")
+	default:
+		fmt.Println("ERROR: You used two flags to set birthdate, each with different data.")
+	}
+}
